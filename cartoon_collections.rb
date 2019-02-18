c20bf4ee773f6c12f5733d9c6b0346481ed68f1e
Alex Merecka <merecka@gmail.com>
2019-02-18 22:32:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,6 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(cheese)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese.find{|x| cheese_types.include?(x)}
   end
